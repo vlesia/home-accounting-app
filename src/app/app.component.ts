8255de1a172702647ccdfbb3ead159fe987a80ee
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-11 23:02:14 +0200

    feat(HA-4.2): create sidebar component and add theme file with color variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatGridListModule,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent implements OnInit {
+  user?: User;
+  ngOnInit(): void {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      this.user = JSON.parse(userData);
+    }
+  }
+}
