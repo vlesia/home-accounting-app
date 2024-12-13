387b590e7d7962a1a1ebc0e007f89ea04bd66b25
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-13 16:59:56 +0200

    feat(HA-4.2): implement responsive design for small devices

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
@@ -19,4 +26,21 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  tabletQuery!: MediaQueryList;
+  private _mobileQueryListener!: () => void;
+
+  private changeDetectorRef = inject(ChangeDetectorRef);
+  private media = inject(MediaMatcher);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.tabletQuery = this.media.matchMedia('(max-width: 960px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.tabletQuery.addEventListener('change', this._mobileQueryListener);
+
+    this.destroyRef.onDestroy(() => {
+      this.tabletQuery.removeEventListener('change', this._mobileQueryListener);
+    });
+  }
+}
