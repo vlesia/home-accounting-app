8bf048230a2cf213d521952ae2c2af56f7df929a
Lesia Hlukhaniuk <156358322+vlesia@users.noreply.github.com>
2024-12-17 09:50:14 +0200

    feat(HA-4.1): create-header-component (#4)
    * feat(HA-4.1): create-header-component
    
    * fix(HA-4.1): add theme file with color variables, improve button styles
    
    * feat(HA-4.1): add redirect to login on user logout
    
    * feat(HA-4.1): update header with decorative buttons
    
    * fix(HA-5): improve project structure
    
    ---------
    
    Co-authored-by: Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
+
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+  },
   {
     path: 'auth',
-    loadChildren: () =>
-      import('./auth/auth.routes').then((m) => m.authRoutes),
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.authRoutes),
   },
 ];
