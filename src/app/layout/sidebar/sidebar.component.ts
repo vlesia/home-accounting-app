480806c898d3886d19f111aa5de5c753b80a1875
Lesia Hlukhaniuk <156358322+vlesia@users.noreply.github.com>
2024-12-18 09:38:56 +0200

    feat(HA-4.2): create sidebar component and add theme file with color … (#5)
    * feat(HA-4.2): create sidebar component and add theme file with color variables
    
    * fix(HA-4.2): change sidebar color and clean up code
    
    * feat(HA-4.2): implement responsive design for small devices
    
    * fix(HA-4.2): improve project structure
    
    ---------
    
    Co-authored-by: Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>

---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent implements OnInit {
+  public user?: User;
+
+  public ngOnInit(): void {
+    const userData = localStorage.getItem('user');
+
+    if (userData) {
+      this.user = JSON.parse(userData);
+    }
+  }
+}
