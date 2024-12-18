2c39446cede82d5d9632721b06e1c7fb8a303d52
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-18 13:25:26 +0200

    feat(HA-6): create billing page

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, map, startWith } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { AsyncPipe } from '@angular/common';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,14 +20,34 @@ import { HeaderComponent } from './header/header.component';
   selector: 'app-layout',
   standalone: true,
   imports: [
+    RouterOutlet,
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
     MatGridListModule,
-    SidebarComponent,
+    MatIconModule,
+    AsyncPipe,
     HeaderComponent,
+    SidebarComponent,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  title$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    startWith(null),
+    map(() => this.getDeepestChild(this.route).snapshot.data['title'] || '')
+  );
+
+  private getDeepestChild(route: ActivatedRoute): ActivatedRoute {
+    let child = route;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    return child;
+  }
+}
