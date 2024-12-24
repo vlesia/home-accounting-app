9f3cdf272265f9fa3d9a97f7c665fd794072e3dd
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-24 16:55:59 +0200

    feat(HA-8.1): create record page

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-record',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatTableModule, MatIconModule],
   templateUrl: './record.component.html',
-  styleUrl: './record.component.scss'
+  styleUrl: './record.component.scss',
 })
 export class RecordComponent {
-
+  public tableColumns: string[] = ['index', 'category', 'limit', 'actions'];
+  public userRecords = [];
 }
