6bd40b98d27c07bd55bdde498a978691aa400570
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-24 16:55:59 +0200

    feat(HA-8.1): create record page

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-record',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatTableModule, MatIconModule],
   templateUrl: './record.component.html',
-  styleUrl: './record.component.scss'
+  styleUrl: './record.component.scss',
 })
-export class RecordComponent {
+export class RecordComponent implements OnInit {
+  public tableColumns: string[] = ['index', 'category', 'limit', 'actions'];
+  public userCategories = []; //Category
 
+  //private historyService = inject(HistoryService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    // const subscription = this.historyService.getCategories().subscribe({
+    //   next: (categories) => (this.userCategories = categories),
+    // });
+
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
