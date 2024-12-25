2d9f5c6840d782615257ed6b678fc5c7e03ac7b4
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-25 21:54:32 +0200

    feat(HA-7.2): add chart-config file

--- src/app/pages/history/spending-chart/spending-chart.component.ts
+++ src/app/pages/history/spending-chart/spending-chart.component.ts
@@ -3,6 +3,7 @@ import * as Highcharts from 'highcharts';
 import { HighchartsChartModule } from 'highcharts-angular';
 
 import { HistoryService } from '../../../services/history.service';
+import { chartOptions } from '../../../config/chart-config';
 
 @Component({
   selector: 'app-spending-chart',
@@ -13,77 +14,28 @@ import { HistoryService } from '../../../services/history.service';
 })
 export class SpendingChartComponent implements OnInit {
   public updateFlag = false;
+  public Highcharts: typeof Highcharts = Highcharts;
+  public chartOptions: Highcharts.Options = chartOptions;
 
   private historyService = inject(HistoryService);
   private destroyRef = inject(DestroyRef);
 
-  public Highcharts: typeof Highcharts = Highcharts;
-  public chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'pie',
-    },
-    colors: [
-      '#9FA8DA',
-      '#7986CB',
-      '#5C6BC0',
-      '#3F51B5',
-      '#1A237E',
-      '#3949AB',
-      '#303F9F',
-    ],
-    title: {
-      text: '',
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: true,
-          formatter: function () {
-            if (this.percentage! >= 10) {
-              return `${this.name}: <b>${this.percentage!.toFixed(1)}%</b>`;
-            }
-
-            return null;
-          },
-          distance: -50,
-          style: {
-            color: 'white',
-            textOutline: 'none',
-          },
-          useHTML: true,
-        },
-      },
-    },
-    series: [
-      {
-        type: 'pie',
-        name: 'Costs',
-        data: [],
-      },
-    ],
-  };
-
   public ngOnInit() {
-    const subscription = this.historyService.getFilteredOutcomeEvents().subscribe({
-      next: (chartData) => {
-        console.log(chartData);
-
-        this.chartOptions.series = [
-          {
-            type: 'pie',
-            name: 'Costs',
-            data: chartData,
-          },
-        ];
-        this.updateFlag = true;
-      },
-      error: (err) => console.error('Error fetching chart data', err),
-    });
+    const subscription = this.historyService
+      .getFilteredOutcomeEvents()
+      .subscribe({
+        next: (chartData) => {
+          this.chartOptions.series = [
+            {
+              type: 'pie',
+              name: 'Costs',
+              data: chartData,
+            },
+          ];
+          this.updateFlag = true;
+        },
+        error: (err) => console.error('Error fetching chart data', err),
+      });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
