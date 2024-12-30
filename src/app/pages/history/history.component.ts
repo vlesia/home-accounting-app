61c7bebed646e009b056502d69098ff318145247
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-12-30 18:25:17 +0200

    Merge branch HA-7.2_implement-chart into main

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
-import { SpendingChartComponent } from "./spending-chart/spending-chart.component";
+import { HistoryTableComponent } from './history-table/history-table.component';
+
+import { SpendingChartComponent } from './spending-chart/spending-chart.component';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [SpendingChartComponent],
+  imports: [HistoryTableComponent, SpendingChartComponent],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrl: './history.component.scss',
 })
-export class HistoryComponent {
-
-}
+export class HistoryComponent {}
