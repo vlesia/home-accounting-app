71c0064bc658a24352584476d6bfa950010f9c9e
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2025-01-07 00:01:45 +0200

    fix(8.3): improve category saving logic

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -2,13 +2,16 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { MatDialog } from '@angular/material/dialog';
-import { filter, Subject, switchMap, takeUntil } from 'rxjs';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { filter, map, Subject, switchMap, takeUntil } from 'rxjs';
 
 import { RecordService } from '../../services/record.service';
 import { ModalFormCategoryComponent } from '../../layout/modal-form-category/modal-form-category.component';
 import { Category } from '../../models/history.model';
 import { HistoryService } from '../../services/history.service';
+import { FormCategory } from '../../models/record.model';
+import { UserService } from './../../services/user.service';
+import { User } from './../../models/user.model';
 
 @Component({
   selector: 'app-record',
@@ -25,6 +28,11 @@ export class RecordComponent implements OnInit, OnDestroy {
   private dialog = inject(MatDialog);
   private recordService = inject(RecordService);
   private historyService = inject(HistoryService);
+  private userService = inject(UserService);
+
+  public get user(): User | null {
+    return this.userService.getUser();
+  }
 
   public ngOnInit(): void {
     this.historyService
@@ -35,19 +43,25 @@ export class RecordComponent implements OnInit, OnDestroy {
       });
   }
   public openFormCategory(): void {
-    const dialogRef = this.dialog.open(ModalFormCategoryComponent, {
-      autoFocus: false,
-      width: '400px',
-      data: {
-        title: 'Add category',
-      },
-    });
+    const dialogRef: MatDialogRef<ModalFormCategoryComponent> =
+      this.dialog.open(ModalFormCategoryComponent, {
+        autoFocus: false,
+        width: '400px',
+        data: {
+          title: 'Add category',
+        },
+      });
     dialogRef
       .afterClosed()
       .pipe(
         takeUntil(this.destroy$),
-        filter((val) => val !== null && val !== undefined),
-        switchMap((val) => this.recordService.saveCategory(val)),
+        filter(Boolean),
+        map((formCategory: FormCategory) => ({
+          name: formCategory.name,
+          capacity: +formCategory.capacity,
+          userId: this.user!.id,
+        })),
+        switchMap((category) => this.recordService.saveCategory(category)),
         switchMap(() => this.historyService.getCategories()),
       )
       .subscribe({
--- src/app/services/record.service.ts
+++ src/app/services/record.service.ts
@@ -2,9 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 
-import { FormCategory } from '../models/record.model';
-import { UserService } from './user.service';
-import { User } from '../models/user.model';
 import { Category } from '../models/history.model';
 
 @Injectable({
@@ -12,18 +9,8 @@ import { Category } from '../models/history.model';
 })
 export class RecordService {
   private http = inject(HttpClient);
-  private userService = inject(UserService);
 
-  public get user(): User | null {
-    return this.userService.getUser();
-  }
-
-  public saveCategory(formCategory: FormCategory): Observable<Category> {
-    const category = {
-      name: formCategory.name,
-      capacity: +formCategory.capacity,
-      userId: this.user!.id,
-    };
+  public saveCategory(category: Omit<Category, 'id'>): Observable<Category> {
     return this.http
       .post<Category>('/categories', category)
       .pipe(
